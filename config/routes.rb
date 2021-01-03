1a50bbcbb210c9129be4ad968776601b896a6063
dawner <dawner.fr@gmail.com>
2021-01-03 11:56:11 -0800

    Add currency to banks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'categories#index'
+
   resources :transaction_batches
   resources :categories
   resources :banks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_225600) do
+ActiveRecord::Schema.define(version: 2021_01_03_928012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_225600) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "currency"
     t.index ["slug"], name: "index_banks_on_slug", unique: true
   end
 
