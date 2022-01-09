3e666fa1a951bbeade92a46492f34f558cbb2273
Dawn Richardson <dawner.fr@gmail.com>
2022-01-09 09:29:22 -0800

    Use tags for updating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.order(:name)
   end
 
   # GET /categories/1
@@ -69,6 +69,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.require(:category).permit(:name, :keywords)
+      params.require(:category).permit(:name, :keywords => [])
     end
 end
